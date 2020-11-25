28e7896ba9f24d49338572b81c6a4f027e48c23a
pierrethiriet <p.thiriet@gmail.com>
2020-11-25 13:43:22 +0100

    dist update

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -95906,7 +95906,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58686" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60323" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
